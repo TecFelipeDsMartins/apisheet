385fac89d1826742a48afa6feb3c81ff60ab2a75
Felipe Martins <tec.felipedsmartins@gmail.com>
2022-03-19 12:06:00 -0300

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+async function hello (req, res) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  var requestOptions = {
+      method: "get",
+      headers: myHeaders,
+      redirect: "follow",
+      
+  }
+
+  const dados = await fetch("https://v1.nocodeapi.com/felipedsmartins/google_sheets/CfihZfsZfyUBuNQT?tabId=perguntas", requestOptions)
+  const dadosJason = await dados.json()
+   
+  res.setHeader('Content-Type', 's-maxage=10, stale-while-revalidate');
+
+  res.json(dadosJason)
+  console.log(dadosJason.data)
+  // res.statusCode = 200
+  // res.json({ name: 'John Doe' })
 }
+
+export default hello
